747f766b1b3c3c8adc703d7d5123c2fe0b611832
Il Tuo Nome <sanbollten2@gmail.com>
2026-07-23 19:12:43 +0200

    Update

--- src/userplugins/vencord-fire-stoker/components/index.ts
+++
@@ -1,9 +0,0 @@
-
-
-
-export * from "./ActivityBadges";
-export * from "./HistoryModal";
-export * from "./Icons";
-export * from "./ProfileCard";
-export * from "./SnapshotsModal";
-export * from "./UserSettings";
